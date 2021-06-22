582904815fb311447f73e0d3411e55f328c3c234
Isfak-Shakil <isfakshakil@gmail.com>
2021-06-22 13:07:42 +0600

    Room Database

---
+++ app/src/main/java/com/example/notetaking/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.notetaking;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+        private NoteViewModel noteViewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        noteViewModel=  new ViewModelProvider(this,
+                ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // update recycler
+            }
+        });
+    }
+}
